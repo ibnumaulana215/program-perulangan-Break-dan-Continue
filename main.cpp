b6b91fc6e5b4d8eea0647e4f6be9360a0c4b7065
ibnu maulana <45716669+ibnumaulana215@users.noreply.github.com>
2018-12-16 18:57:24 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,12 +3,19 @@
 
 using namespace std;
 
-int main()
+int main ()
 {
- for (int i=1 ;i<50; i++){
-        if(i==20) break;
+for (int i=1;i<10;i++){
+        if(i==5)continue;
         printf("%d\n",i);
+
     }
 
+  printf("\n");
 
+
+    for (int y=1; y<50 ; y++){
+            if(y==20) break;
+        printf("%d\n",y);
+    }
 }
